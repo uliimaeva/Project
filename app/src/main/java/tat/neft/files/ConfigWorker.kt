cc2f0428db5495fa5f1859023949e11c55e8914c
uliimaeva <i.julka14@yandex.ru>
2023-08-03 21:03:29 +0300

    added class to work with config

---
+++ app/src/main/java/tat/neft/files/ConfigWorker.kt
@@ -0,0 +1,52 @@
+package tat.neft.files
+
+import android.content.ContentValues
+import android.content.Context
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Toast
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.io.File
+import java.io.FileInputStream
+import java.io.OutputStreamWriter
+
+class ConfigWorker(val context: Context) {
+    val CONFIG_NAME: String = "config.json"
+    val CONFIG_PATH: String = "/storage/emulated/0/Download/" + CONFIG_NAME
+    val DEFAULT_CONFIG: MutableList<MyFile> = mutableListOf(MyFile("", "Test App", "Test App Title!"))
+
+    fun configExists(): Boolean {
+        return File(CONFIG_PATH).exists()
+    }
+
+    fun readConfig(): MutableList<MyFile> {
+        if (!configExists()) {
+            dumpConfig(DEFAULT_CONFIG)
+            return DEFAULT_CONFIG
+        }
+
+        val text = FileInputStream(File(CONFIG_PATH)).bufferedReader().use { it.readText() }
+        return Json.decodeFromString(text)
+    }
+
+    fun dumpConfig(values: MutableList<MyFile>) {
+        val json = Json.encodeToString(values)
+        File(CONFIG_PATH).writeBytes(json.toByteArray())
+    }
+
+    fun optionExists(option: MyFile): Boolean {
+        return readConfig().filter { f -> f.url == option.url }.isNotEmpty()
+    }
+
+    fun addOption(option: MyFile) {
+        val newConfig = readConfig().filter { f -> f.url != option.url }.toMutableList()
+        newConfig.add(option)
+        dumpConfig(newConfig)
+    }
+
+    fun deleteOption(option: MyFile) {
+        dumpConfig(readConfig().filter { f -> f.url != option.url }.toMutableList())
+    }
+}
