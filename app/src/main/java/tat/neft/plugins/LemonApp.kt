6f1d318eeb5c712442ab9e3be312ce7afa0ca5ec
work <i.julka14@gmail.com>
2023-08-10 16:42:15 +0300

    2 new apps

--- app/src/main/java/tat/neft/plugins/LemonApp.kt
+++ app/src/main/java/tat/neft/plugins/LemonApp.kt
@@ -1,14 +1,13 @@
 package tat.neft.plugins
 
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.widget.ImageSwitcher
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
@@ -21,47 +20,74 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import tat.neft.R
 
-class TextApp : ComponentActivity() {
+class LemonApp: ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContent{
-            App()
+        setContent {
+            Lemon()
         }
     }
 }
 
 @Composable
-fun App(modifier: Modifier = Modifier) {
+fun Lemon(modifier: Modifier = Modifier) {
+
     var result  by remember {
-        mutableStateOf(0)
+        mutableStateOf(1)
+    }
+    var item  by remember {
+        mutableStateOf(1)
     }
 
+    var click = 0
+
     val imageResource = when (result) {
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        else -> R.drawable.dice_6
+        1 -> R.drawable.lemon_tree
+        2 -> R.drawable.lemon_squeeze
+        3 -> R.drawable.lemon_drink
+        else -> R.drawable.lemon_restart
     }
-    Surface(modifier = Modifier.fillMaxSize(), color = Color.White) {
+    val text = when (result) {
+        1 -> "Нажми чтобы выбрать лимон"
+        2 -> "Нажимай пока не выжмешь сок"
+        3 -> "Нажми чтобы выпить сок"
+        else -> "Нажми чтобы сделать сок еще раз"
+    }
+
+
+    Surface(modifier = Modifier.fillMaxSize(), color = Color.White){
         Column(
             modifier = modifier,
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            Image(painter = painterResource(id = imageResource), contentDescription = result.toString())
-            Button(onClick = { result = (1..6).random() }) {
-                Text(text = result.toString())
+            Button(onClick = {
+                if (result == 4){
+                    result = 1
+                    item = (2..4).random()
+                }
+                else {
+                    if (result == 2){
+                        if (item == click) { result ++}
+                        else {click++}
+                    }
+                    else {result ++}
+                }
+
+            }, Modifier.size( 300.dp, 300.dp)
+            ) {
+                Image(painter = painterResource(id = imageResource), contentDescription = result.toString())
             }
+            Text(text = text, Modifier.padding(15.dp))
         }
     }
 }
@@ -70,5 +96,5 @@ fun App(modifier: Modifier = Modifier) {
 @Preview
 @Composable
 private fun AppPreview(){
-    App()
+    Lemon()
 }
